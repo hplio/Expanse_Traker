acab882458291669bb36425f163fa5d621802cd8
hplio <harshilpatel9724@gmail.com>
2025-05-22 19:26:30 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import "@tailwindcss/postcss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Expanse_Traker/',
-  plugins: [tailwindcss(), react()],
+  base: "/Expanse_Traker/",
+  plugins: [react(), tailwindcss()],
 });
